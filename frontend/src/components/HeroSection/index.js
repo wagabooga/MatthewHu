358f578ccd081fcdd701542d2fe52e014414d134
wagabooga <mattyhutwo@gmail.com>
2022-01-10 23:46:07 -0800

    added video to main feed part of page

---
+++ frontend/src/components/HeroSection/index.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react'
+import Video from '../../videos/video.mp4'
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight} from './HeroElements';
+
+const HeroSection = () => {
+  const [hover, setHover] = useState(false)
+
+  const onHover = () => {
+    setHover(!hover)
+  }
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>
+          Header here ASDADASD
+        </HeroH1>
+        <HeroP>
+          Sign up today and win!
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
+            Get Started { hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  )
+}
+
+export default HeroSection
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import Sidebar from '../components/Sidebar'
 import Navbar from '../components/Navbar'
+import HeroSection from '../components/HeroSection'
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState('')
@@ -12,6 +13,7 @@ const Home = () => {
     <>
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
+      <HeroSection />
     </>
   )
 }
