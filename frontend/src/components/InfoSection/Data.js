765b9c3561fdee30b390646993094b8432f2f48f
wagabooga <mattyhutwo@gmail.com>
2022-01-12 19:08:53 -0800

    Added images and created basic info section

---
+++ frontend/src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Full Stack Web Developer',
+  headline: 'Info asdasdasdasdasdasda asd asd asd asd asd asd ',
+  description: ' asd asd asd asd asd asdasdasdas asd asd ',
+  buttonLabel: 'Get started',
+  imgStart: false,
+  img: require('../../images/svg-2.svg').default,
+  alt: 'Car',
+  dark: true,
+  primary: true,
+  darkText: false
+}
