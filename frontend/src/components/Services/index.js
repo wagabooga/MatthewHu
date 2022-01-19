6165b2c1f24a3b386bd70f83c4d07216522a983b
Matthew Hu <wagabooga@Matthews-MacBook-Pro.local>
2022-01-19 07:08:27 -0800

    Added images and descriptions for each project.

--- frontend/src/components/Services/index.js
+++ frontend/src/components/Services/index.js
@@ -1,26 +1,37 @@
 import React from 'react'
-import Icon1 from '../../images/svg-1.svg'
-import Icon2 from '../../images/svg-2.svg'
-import Icon3 from '../../images/svg-3.svg'
+import BlpImg from '../../images/BLP-main.jpeg'
+import SpotterImg from '../../images/Spotter-mobile.png'
+import SchedulerImg from '../../images/Scheduler-main.png'
 import { ServicesContainer, ServicesH1, ServicesWrapper, ServicesCard, ServicesIcon, ServicesH2, ServicesP } from './ServicesElements'
 
 const Services = () => {
+  let redirectScheduler = function () {
+    window.location.replace("https://hardcore-wilson-719e0a.netlify.app/")
+  }
+
+  let redirectSpotter = function () {
+    window.location.replace("https://github.com/wagabooga/Spotter")
+  }
+  let redirectBLP = function () {
+    window.location.replace("https://github.com/wagabooga/Bucket-List-Project")
+  }
+
   return (
     <ServicesContainer id="projects">
       <ServicesH1> My Projects </ServicesH1>
       <ServicesWrapper>
-        <ServicesCard>
-          <ServicesIcon src={Icon1}/>
+        <ServicesCard onClick={redirectBLP} >
+          <ServicesIcon src={BlpImg}  />
           <ServicesH2>Bucket-List-Project</ServicesH2>
           <ServicesP>Currently a work in progress! Come back soon!</ServicesP>
         </ServicesCard>
-        <ServicesCard>
-          <ServicesIcon src={Icon2}/>
+        <ServicesCard onClick={redirectScheduler}>
+          <ServicesIcon src={SchedulerImg}/>
           <ServicesH2>Scheduler</ServicesH2>
           <ServicesP>Simple CRUD app where you can schedule interviews</ServicesP>
         </ServicesCard>
-        <ServicesCard>
-          <ServicesIcon src={Icon3}/>
+        <ServicesCard onClick={redirectSpotter}>
+          <ServicesIcon src={SpotterImg}/>
           <ServicesH2>Spotter</ServicesH2>
           <ServicesP>Social-media app that combines Twitter and Spotify</ServicesP>
         </ServicesCard>
